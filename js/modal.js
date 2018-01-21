429a40ea02802103cdc4d83317c2c715edca290d
Satish Kumar <satish.kumar@marketale.com>
2018-01-21 17:27:12 +0530

    Added CSS, JS and image files

---
+++ js/modal.js
@@ -0,0 +1,6 @@
+//Javascript Source: http://jsfiddle.net/jschr/3kgbG/
+
+$('.launchConfirm').on('click', function (e) {
+    $('#confirm')
+        .modal({ backdrop: 'static', keyboard: false })
+});
